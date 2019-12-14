158777c79671df68adc2968a0ee7f7ac0ac7724e
Jonathan <33239505+jonathanssm@users.noreply.github.com>
2019-12-14 14:03:01 -0300

    v1.0.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// Paginas
+import { HomeComponent } from './paginas/home/home.component';
+import { ContatosComponent } from './paginas/contatos/contatos.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent, data: { title: 'Home' } },
+  { path: 'contatos', component: ContatosComponent, data: { title: 'Contatos'} }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Terceiros
+import { NgxMaskInputModule } from 'ngx-mask-input';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+
+// Componentes
+import { HomeComponent } from './paginas/home/home.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { ContatosComponent } from './paginas/contatos/contatos.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    ContatosComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    NgxMaskInputModule,
+    BrowserAnimationsModule,
+    BsDatepickerModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
